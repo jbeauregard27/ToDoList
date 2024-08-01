f3718b7e07519e645ab88d6176049e1b9a56a10a
Josh Beauregard <joshuaebeauregard@outlook.com>
2024-08-01 16:15:17 -0400

    it works now

--- conversion.js
+++ conversion.js
@@ -1,16 +1,20 @@
+document.getElementById("convert").addEventListener("click", function () {
+    convertTemperature()
 
-let userTemp = document.getElementById("temp").value;
+});
 
-let fahrenheit = userTemp;
-// Calculates Temperature in Celsius from Fahrenheit
-let celsius = fahrenheit - 32 * 5 / 9;
-//Calculates Temperature in Kelvin from Celsius
-let kelvin = celsius + 273;
-fahrenheit = Math.floor(fahrenheit);
-//Calclulates Temperature in Newton from Celsius
-let newton = celsius * (33/100);
-newton = Math.floor(newton);
-
-console.log(`The temperature in Fahrenheit is ${fahrenheit}`);
-console.log(`The temperature in Kelvin is ${kelvin}`);
-console.log(`THe temperature in Newton is ${newton}`);
+function convertTemperature() {
+    let userTemp = document.getElementById("temp").value;
+    let celsiushtml = document.getElementById("cels")
+    let celsvalue = Math.floor(userTemp - 32 * 5 / 9);
+    celsiushtml.innerHTML = celsvalue;
+    console.log(userTemp);
+    let kelvinhtml = document.getElementById("kelv")
+    let kelvvalue = celsvalue + 273;
+    kelvinhtml.innerHTML = kelvvalue;
+    console.log(kelvvalue);
+    let newtonhtml = document.getElementById("newt")
+    let newtvalue = celsvalue * 33 / 100;
+    newtonhtml.innerHTML = newtvalue;
+    console.log(newtvalue);
+}
